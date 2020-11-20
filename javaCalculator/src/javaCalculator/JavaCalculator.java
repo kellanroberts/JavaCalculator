e400b7aefd1959f8b34e2354d8023945cfde3c06
kellanroberts <73409978+kellanroberts@users.noreply.github.com>
2020-11-20 18:03:29 -0500

    Add files via upload

---
+++ javaCalculator/src/javaCalculator/JavaCalculator.java
@@ -0,0 +1,20 @@
+package javaCalculator;
+
+public class JavaCalculator {
+	public double add(int a, int b) {
+		return (double) a+b;
+	}
+	public double subtract(int a, int b) {
+		return (double) a-b;
+	}
+	public double multiply(int a, int b) {
+		return (double) a*b;
+	}
+	public double divide(int a, int b) {
+		return (double) a/b;
+	}
+	public double square(int a) {
+		return (double) a*a;
+	}
+
+}
---
+++ javaCalculator/src/javaCalculator/Math.java
@@ -0,0 +1,45 @@
+package javaCalculator;
+
+public class Math() {
+	 public static <String> void main(String args[], double a) 
+	    { 
+	  
+	        double degrees = a; 
+	  
+	        double radians = Math.toRadians(degrees); 
+	  
+	        double sinValue = Math.sin(radians); 
+	  
+	        System.out.println("sin(" + degrees + ") = " + sinValue); 
+	    }
+
+	private static double sin(double radians) {
+		return sin(radians);
+	}
+
+	private static double toRadians(double degrees) {
+		return 0;
+	} 
+	
+	public static double sqrt(double a) {
+		return sqrt(a);
+	}
+	
+	public static double cos(double a) {
+		return cos(a);
+	}
+	
+	public static double tan(double a) {
+		return tan(a);
+	}
+	
+	public static double factorial(double a) {
+		return factorial(a);
+	}
+
+	
+
+
+	
+
+
